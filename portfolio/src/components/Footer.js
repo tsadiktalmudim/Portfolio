3fa53be8b101fb1524aae01eb80bffc314587fe7
Xan <tsadiktalmudim@gmail.com>
2022-07-21 21:37:16 -0500

    footer completed

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,31 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebookSquare,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
-    <footer className="footer-pin">
-      <div className="footer">
-        <div>
-          <h5>
-            © All code by
-            <p>Xan Hamilton</p>
-          </h5>
-        </div>
-        <div>
-          <ul className="social-menu">
-            <li>
-              <a href="https://www.facebook.com/xan.hamilton.7/">
-                <FontAwesomeIcon className="i" icon={faFacebookSquare}/>
-              </a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/thomas-hamilton-522b17181/">
-                <FontAwesomeIcon className="i" icon={faLinkedin} />
-              </a>
-            </li>
-          </ul>
+    <footer>
+      <div className="footer-pin">
+        <h5>
+          © All code by <br />
+          Xan Hamilton
+        </h5>
+      </div>
+      <div className="righty">
+        <div className="social-menu">
+          <a href="https://www.facebook.com/xan.hamilton.7/">
+            <FontAwesomeIcon
+              className="i fa-facebook"
+              icon={faFacebookSquare}
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/thomas-hamilton-522b17181/">
+            <FontAwesomeIcon className="i" icon={faLinkedin} />
+          </a>
         </div>
       </div>
     </footer>
